4ee8bdf8554aaa1c5df94395798088a8e2c85928
Riken7 <rikenprajapati2005@gmail.com>
2024-10-31 18:09:08 +0530

    ...

--- lua/rik/plugins/lsp/lspconfig.lua
+++ lua/rik/plugins/lsp/lspconfig.lua
@@ -6,15 +6,15 @@ return {
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 		{ "folke/neodev.nvim", opts = {} },
 	},
-	config = function()
-		local lspconfig = require("lspconfig")
-		--    local mason_lspconfig = require("mason-lspconfig")
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-		local keymap = vim.keymap -- for conciseness
+  config = function()
+  	local lspconfig = require("lspconfig")
+  	--    local mason_lspconfig = require("mason-lspconfig")
+  	local cmp_nvim_lsp = require("cmp_nvim_lsp")
+  	local keymap = vim.keymap -- for conciseness
 
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-			callback = function(ev)
+    vim.api.nvim_create_autocmd("LspAttach", {
+	  	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  		callback = function(ev)
 				-- Buffer local mappings
 				local opts = { buffer = ev.buf, silent = true }
 
@@ -60,122 +60,117 @@ return {
 
 		-- Autocompletion capabilities
 		--local capabilities = cmp_nvim_lsp.default_capabilities()
+  	-- Diagnostic symbols
+	  local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+    for type, icon in pairs(signs) do
+  		local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+  	end
+  	local lua_ls_path = "/nix/store/3fp5s0cydnczsh74ylsqni2c3kiapx4s-lua-language-server-3.10.6/bin/lua-language-server" 
+    lspconfig.lua_ls.setup({
+	  	cmd = { lua_ls_path },
+      capabilities = cmp_nvim_lsp.default_capabilities(),
+  		settings = {
+  			Lua = {
+  				diagnostics = {
+  					globals = { "vim" },
+  				},
+  				format = {
+  					enable = true,
+  				},
+  			},
+  		},
+  	})
+  	local jdtls_path = "/nix/store/smcs8wil4sdwn1lgfpir3yzbnfa6nmrc-jdt-language-server-1.39.0/bin/jdtls"
+  	lspconfig.jdtls.setup({
+  		cmd = { jdtls_path, "--add-modules", "java.se", "--add-exports", "java.base/java.lang=ALL-UNNAMED" },
+  		root_dir = function(fname)
+  			local root_patterns = { ".git", "gradlew", "mvnw" }
+  			local found = vim.fs.find(root_patterns, { upward = true })
+  			if #found > 0 then
+  				return vim.fs.dirname(found[1])
+  			else
+  				return vim.fs.dirname(fname)
+  			end
+  		end,
+  		capabilities = vim.tbl_deep_extend(
+  			"force",
+  			vim.lsp.protocol.make_client_capabilities(),
+  			require("cmp_nvim_lsp").default_capabilities()
+  		),
+  		filetypes = { "java" },
+  		settings = {
+  			java = {
+  				signatureHelp = { enabled = true },
+  				contentProvider = { preferred = "fernflower" },
+  				import = { enabled = true },
+  				rename = { enabled = true },
+  			},
+  		},
+  	})
+  	lspconfig.clangd.setup({
+  		cmd = { clangd_path },
+  		capabilities = cmp_nvim_lsp.default_capabilities(),
+  		filetypes = { "c", "cpp", "objc", "objcpp" },
+  		settings = {
+  			clangd = {
+  				--additional settings
+  			},
+  		},
+  	})
 
-		-- Diagnostic symbols
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
-		local lua_ls_path =
-     "/nix/store/3fp5s0cydnczsh74ylsqni2c3kiapx4s-lua-language-server-3.10.6/bin/lua-language-server" 
-		lspconfig.lua_ls.setup({
-			cmd = { lua_ls_path },
-			capabilities = cmp_nvim_lsp.default_capabilities(),
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-					format = {
-						enable = true,
-					},
-				},
+	  local rust_path = "/nix/store/0fdrbfqza9p1vwzccy1hb1pdbx55hags-rust-analyzer-2024-09-02/bin/rust-analyzer"
+	  lspconfig.rust_analyzer.setup({
+	  	on_attach = function(client, bufnr)
+	  		vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+	  	end,
+	  	cmd = { rust_path },
+	  	capabilities = cmp_nvim_lsp.default_capabilities(),
+	  	filetypes = { "rust" },
+	  	root_dir = lspconfig.util.root_pattern("Cargo.toml", ".git"),
+	  	settings = {
+	  		["rust-analyzer"] = {
+	  			cargo = {
+	  				buildScripts = { enable = true },
+	  				allFeatures = true,
+	  			},
+	  			imports = {
+	  				granularity = { group = "module" },
+	  			},
+	  			procMacro = { enable = true },
+	  		},
+	  	},
+	  })
+	  local ts_path = "/nix/store/rv4pwdwp3axbdip0l4fq0i8sa1caws3k-typescript-language-server-4.3.3/bin/typescript-language-server"
+		lspconfig.ts_ls.setup({ 
+      cmd = { ts_path, "--stdio" },
+		  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+		  root_dir = lspconfig.util.root_pattern("tsconfig.json", "jsconfig.json", ".git"),
+		  capabilities = cmp_nvim_lsp.default_capabilities(),
+		  settings = {
+		  	typescript = {
+		  		--additional settings
+		  	},
+		  	javascript = {
+		  		--additional settings
+		  	},
 			},
 		})
-		local jdtls_path = "/nix/store/smcs8wil4sdwn1lgfpir3yzbnfa6nmrc-jdt-language-server-1.39.0/bin/jdtls"
-
-		lspconfig.jdtls.setup({
-			cmd = { jdtls_path, "--add-modules", "java.se", "--add-exports", "java.base/java.lang=ALL-UNNAMED" },
-			root_dir = function(fname)
-				local root_patterns = { ".git", "gradlew", "mvnw" }
-				local found = vim.fs.find(root_patterns, { upward = true })
-				if #found > 0 then
-					return vim.fs.dirname(found[1])
-				else
-					return vim.fs.dirname(fname)
-				end
-			end,
-			capabilities = vim.tbl_deep_extend(
-				"force",
-				vim.lsp.protocol.make_client_capabilities(),
-				require("cmp_nvim_lsp").default_capabilities()
-			),
-			filetypes = { "java" },
-			settings = {
-				java = {
-					signatureHelp = { enabled = true },
-					contentProvider = { preferred = "fernflower" },
-					import = { enabled = true },
-					rename = { enabled = true },
-				},
-			},
-		})
-		lspconfig.clangd.setup({
-			cmd = { clangd_path },
-			capabilities = cmp_nvim_lsp.default_capabilities(),
-			filetypes = { "c", "cpp", "objc", "objcpp" },
-			settings = {
-				clangd = {
-					--additional settings
-				},
-			},
-		})
-
-		local rust_path = "/nix/store/c9mv63b3xck3kacmfir5dzf408kxjq41-rust-analyzer-2024-08-12/bin/rust-analyzer"
-		lspconfig.rust_analyzer.setup({
-			on_attach = function(client, bufnr)
-				vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-			end,
-			cmd = { rust_path },
-			capabilities = cmp_nvim_lsp.default_capabilities(),
-			filetypes = { "rust" },
-			root_dir = lspconfig.util.root_pattern("Cargo.toml", ".git"),
-			settings = {
-				["rust-analyzer"] = {
-					cargo = {
-						buildScripts = { enable = true },
-						allFeatures = true,
-					},
-					imports = {
-						granularity = { group = "module" },
-					},
-					procMacro = { enable = true },
-				},
-			},
-		})
-		local ts_path =
-			"/nix/store/rv4pwdwp3axbdip0l4fq0i8sa1caws3k-typescript-language-server-4.3.3/bin/typescript-language-server"
-		lspconfig.ts_ls.setup({
-			cmd = { ts_path, "--stdio" },
-			filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-			root_dir = lspconfig.util.root_pattern("tsconfig.json", "jsconfig.json", ".git"),
-			capabilities = cmp_nvim_lsp.default_capabilities(),
-			settings = {
-				typescript = {
-					--additional settings
-				},
-				javascript = {
-					--additional settings
-				},
-			},
-		})
-
-		lspconfig.pyright.setup({
-			capabilities = cmp_nvim_lsp.default_capabilities(),
-			settings = {
-				python = {
-					analysis = {
-						typeCheckingMode = "strict", -- Customize this as needed
-					},
-				},
-			},
-		})
-		local tailwindcss_path = "/nix/store/fzlkpk5p2w7piqc6vvxvky4zbswc1fms-tailwindcss-language-server-0.0.21/bin/tailwindcss-language-server"
-		lspconfig.tailwindcss.setup({
-			cmd = { tailwindcss_path, "--stdio" },
-			filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact" },
-			capabilities = cmp_nvim_lsp.default_capabilities(),
-		})
-	end,
+  	lspconfig.pyright.setup({
+  		capabilities = cmp_nvim_lsp.default_capabilities(),
+  		settings = {
+  			python = {
+  				analysis = {
+  					typeCheckingMode = "strict", -- Customize this as needed
+  				},
+  			},
+  		},
+  	})
+  	local tailwindcss_path = "/nix/store/fzlkpk5p2w7piqc6vvxvky4zbswc1fms-tailwindcss-language-server-0.0.21/bin/tailwindcss-language-server"
+  	lspconfig.tailwindcss.setup({
+  		cmd = { tailwindcss_path, "--stdio" },
+  		filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  		capabilities = cmp_nvim_lsp.default_capabilities(),
+  	})
+  end,
 }
